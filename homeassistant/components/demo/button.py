1cd5c3221c6980f08ec61a537c8843ff76d4fda3
Akash <akashadike15@gmail.com>
2023-10-05 22:37:48 +0200

    Removed unused parameters config_entry and hass from the function async_setup_entry() Location: hoomeassistant/components/demo/button.py

--- homeassistant/components/demo/button.py
+++ homeassistant/components/demo/button.py
@@ -12,8 +12,6 @@ from . import DOMAIN
 
 
 async def async_setup_entry(
-    hass: HomeAssistant,
-    config_entry: ConfigEntry,
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the demo button platform."""
