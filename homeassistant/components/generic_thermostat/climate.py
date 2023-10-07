b6bdb5e27ba8d892950fa1dea979fca5fc308e5a
Akash <akashadike15@gmail.com>
2023-10-07 15:29:49 +0200

    Reduced cognitive complexity of the function async_added_to_hass() -> None in generic_thermostat/climate.py

--- homeassistant/components/generic_thermostat/climate.py
+++ homeassistant/components/generic_thermostat/climate.py
@@ -234,40 +234,29 @@ class GenericThermostat(ClimateEntity, RestoreEntity):
         """Run when entity about to be added."""
         await super().async_added_to_hass()
 
-        # Add listener
         self.async_on_remove(
-            async_track_state_change_event(
-                self.hass, [self.sensor_entity_id], self._async_sensor_changed
-            )
+            async_track_state_change_event(self.hass, [self.sensor_entity_id], self._async_sensor_changed)
         )
         self.async_on_remove(
-            async_track_state_change_event(
-                self.hass, [self.heater_entity_id], self._async_switch_changed
-            )
+            async_track_state_change_event(self.hass, [self.heater_entity_id], self._async_switch_changed)
         )
 
         if self._keep_alive:
             self.async_on_remove(
-                async_track_time_interval(
-                    self.hass, self._async_control_heating, self._keep_alive
-                )
+                async_track_time_interval(self.hass, self._async_control_heating, self._keep_alive)
             )
 
         @callback
         def _async_startup(*_):
             """Init on startup."""
             sensor_state = self.hass.states.get(self.sensor_entity_id)
-            if sensor_state and sensor_state.state not in (
-                STATE_UNAVAILABLE,
-                STATE_UNKNOWN,
-            ):
+            switch_state = self.hass.states.get(self.heater_entity_id)
+
+            if sensor_state and sensor_state.state not in (STATE_UNAVAILABLE, STATE_UNKNOWN):
                 self._async_update_temp(sensor_state)
                 self.async_write_ha_state()
-            switch_state = self.hass.states.get(self.heater_entity_id)
-            if switch_state and switch_state.state not in (
-                STATE_UNAVAILABLE,
-                STATE_UNKNOWN,
-            ):
+
+            if switch_state and switch_state.state not in (STATE_UNAVAILABLE, STATE_UNKNOWN):
                 self.hass.create_task(self._check_switch_initial_state())
 
         if self.hass.state == CoreState.running:
@@ -279,40 +268,27 @@ class GenericThermostat(ClimateEntity, RestoreEntity):
         if (old_state := await self.async_get_last_state()) is not None:
             # If we have no initial temperature, restore
             if self._target_temp is None:
-                # If we have a previously saved temperature
-                if old_state.attributes.get(ATTR_TEMPERATURE) is None:
-                    if self.ac_mode:
-                        self._target_temp = self.max_temp
-                    else:
-                        self._target_temp = self.min_temp
-                    _LOGGER.warning(
-                        "Undefined target temperature, falling back to %s",
-                        self._target_temp,
-                    )
-                else:
+                self._target_temp = self.max_temp if self.ac_mode else self.min_temp
+                if old_state.attributes.get(ATTR_TEMPERATURE) is not None:
                     self._target_temp = float(old_state.attributes[ATTR_TEMPERATURE])
-            if (
-                self.preset_modes
-                and old_state.attributes.get(ATTR_PRESET_MODE) in self.preset_modes
-            ):
+
+            if self.preset_modes and old_state.attributes.get(ATTR_PRESET_MODE) in self.preset_modes:
                 self._attr_preset_mode = old_state.attributes.get(ATTR_PRESET_MODE)
+
             if not self._hvac_mode and old_state.state:
                 self._hvac_mode = old_state.state
 
         else:
             # No previous state, try and restore defaults
             if self._target_temp is None:
-                if self.ac_mode:
-                    self._target_temp = self.max_temp
-                else:
-                    self._target_temp = self.min_temp
-            _LOGGER.warning(
-                "No previously saved temperature, setting to %s", self._target_temp
-            )
+                self._target_temp = self.max_temp if self.ac_mode else self.min_temp
+            _LOGGER.warning("No previously saved temperature, setting to %s", self._target_temp)
 
         # Set default state to off
         if not self._hvac_mode:
             self._hvac_mode = HVACMode.OFF
+    
+
 
     @property
     def precision(self):
