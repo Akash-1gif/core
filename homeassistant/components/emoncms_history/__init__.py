9beae6d3cc727d5d4294e2e54ac216073f8154be
Akash <akashadike15@gmail.com>
2023-10-07 14:39:28 +0200

    Reduced cognitive complexity of the function: setup() -> True. Location: homeassistant/components/emoncms_history/__init__.py.py

--- homeassistant/components/emoncms_history/__init__.py
+++ homeassistant/components/emoncms_history/__init__.py
@@ -53,52 +53,26 @@ def setup(hass: HomeAssistant, config: ConfigType) -> bool:
             fullurl = f"{url}/input/post.json"
             data = {"apikey": apikey, "data": payload}
             parameters = {"node": node}
-            req = requests.post(
-                fullurl, params=parameters, data=data, allow_redirects=True, timeout=5
-            )
+            req = requests.post(fullurl, params=parameters, data=data, allow_redirects=True, timeout=5)
 
         except requests.exceptions.RequestException:
             _LOGGER.error("Error saving data '%s' to '%s'", payload, fullurl)
-
         else:
             if req.status_code != HTTPStatus.OK:
-                _LOGGER.error(
-                    "Error saving data %s to %s (http status code = %d)",
-                    payload,
-                    fullurl,
-                    req.status_code,
-                )
+                _LOGGER.error("Error saving data %s to %s (http status code = %d)", payload, fullurl, req.status_code)
 
-    def update_emoncms(time):
+    def update_emoncms():
         """Send whitelisted entities states regularly to Emoncms."""
-        payload_dict = {}
-
-        for entity_id in whitelist:
-            state = hass.states.get(entity_id)
-
-            if state is None or state.state in (STATE_UNKNOWN, "", STATE_UNAVAILABLE):
-                continue
-
-            try:
-                payload_dict[entity_id] = state_helper.state_as_number(state)
-            except ValueError:
-                continue
+        payload_dict = {entity_id: state_helper.state_as_number(hass.states.get(entity_id))
+                        for entity_id in whitelist
+                        if (state := hass.states.get(entity_id)) and state.state not in (STATE_UNKNOWN, "", STATE_UNAVAILABLE)}
 
         if payload_dict:
-            payload = "{%s}" % ",".join(
-                f"{key}:{val}" for key, val in payload_dict.items()
-            )
+            payload = "{%s}" % ",".join(f"{key}:{val}" for key, val in payload_dict.items())
 
-            send_data(
-                conf.get(CONF_URL),
-                conf.get(CONF_API_KEY),
-                str(conf.get(CONF_INPUTNODE)),
-                payload,
-            )
+            send_data(conf.get(CONF_URL), conf.get(CONF_API_KEY), str(conf.get(CONF_INPUTNODE)), payload)
 
-        track_point_in_time(
-            hass, update_emoncms, time + timedelta(seconds=conf.get(CONF_SCAN_INTERVAL))
-        )
+        track_point_in_time(hass, update_emoncms, dt_util.utcnow() + timedelta(seconds=conf.get(CONF_SCAN_INTERVAL)))
 
-    update_emoncms(dt_util.utcnow())
+    update_emoncms()
     return True
